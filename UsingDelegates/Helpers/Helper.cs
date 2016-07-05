28982d0d7371fc0cf143df4d6e5516ecf415cfe7
Sirwan Afifi <Sirwan Afifi>
2016-07-05 23:41:45 +0430

    First commit

---
+++ UsingDelegates/Helpers/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using Antlr.Runtime.Misc;
+
+namespace UsingDelegates.Helpers
+{
+    public static class Helper
+    {
+        // public delegate HelperResult ItemTemplate<T>(T input);
+
+        public static HelperResult Repeater<T>(this HtmlHelper html,
+          IEnumerable<T> items,
+          Func<T, HelperResult> itemTemplate,
+          Func<T, HelperResult> alternatingitemTemplate = null)
+        {
+            return new HelperResult(writer =>
+            {
+                int i = 0;
+                foreach (var item in items)
+                {
+                    var func = i % 2 == 0 ? itemTemplate : alternatingitemTemplate;
+                    func(item).WriteTo(writer);
+                    i++;
+                }
+            });
+        }
+    }
+}
---
+++ UsingDelegates/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace UsingDelegates.Models
+{
+    public class Product
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+
+    public class Products : List<Product>
+    {
+        public Products()
+        {
+            this.Add(new Product { Title = "Titl1", Price = 50000 });
+            this.Add(new Product { Title = "Titl2", Price = 55000 });
+            this.Add(new Product { Title = "Titl3", Price = 88000 });
+            this.Add(new Product { Title = "Titl4", Price = 23000 });
+            this.Add(new Product { Title = "Titl5", Price = 150000 });
+        }
+    }
+}
